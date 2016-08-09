6787bfd98013f1c088218c07c46facf6136bc1f8
Vitaliy <kaktusyaka@gmail.com>
2016-08-09 15:43:27 +1000

    Update interval for contacting snitch. Now it's every 10 minutes

--- app/workers/sidekiq/snitch.rb
+++ app/workers/sidekiq/snitch.rb
@@ -13,7 +13,7 @@ module Sidekiq
         Net::HTTP.get(URI(ENV['SIDEKIQ_SNITCH_URL']))
 
         # groundhog day!
-        Snitch.perform_in(15.minutes)
+        Snitch.perform_in(10.minutes)
       end
     end
   end
